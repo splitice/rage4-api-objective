f6874e2a04f4bf155b9e6b49548f3a3ea9987d34
splitice <mat999@gmail.com>
2014-06-14 21:09:51 +1000

    Initial commit of a testing framework and travis definition

---
+++ tests/SimpleRecordTest.php
@@ -0,0 +1,31 @@
+<?php
+use SplitIce\Rage4\Domain;
+use Splitice\Rage4\Rage4Api;
+
+/**
+ * Created by PhpStorm.
+ * User: splitice
+ * Date: 6/4/14
+ * Time: 2:24 PM
+ */
+
+class SimpleDomainTest extends PHPUnit_Framework_TestCase {
+    const API_CLIENT = '\\Splitice\\Rage4\\IRage4ApiClient';
+
+    function testCreateDomain(){
+        //Setup
+        $name = "test.com";
+        $email = "email@test.com";
+        $ns = "ns";
+        $type = Domain::TYPE_DOMAIN;
+
+        //Assert
+        $client = $this->getMock(self::API_CLIENT);
+        $client->expects($this->once())->method('executeApi')->with($this->equalTo('createregulardomainext'),$this->equalTo(array('name'=>$name,'email'=>$email,'ns'=>$ns)));
+
+        //Do
+        $api = new Rage4Api($client);
+        $domain = new Domain($name, $type, $email, array(), $ns);
+        $domain->create($api);
+    }
+} 
